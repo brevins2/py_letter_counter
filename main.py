f1fc4cd1b26d70201fcb13f36602b88f8de126fe
Kiggundu Brevin <74289360+brevins2@users.noreply.github.com>
2023-07-21 05:56:47 +0300

    Add files via upload
    Numeric and alphanumeric

--- main.py
+++ main.py
@@ -1,23 +1,93 @@
-def letter_counter():
-    para = input("Enter paragraph/sentences to get count ")
-    array_of_letters(para)
+para = input("Enter paragraph/sentences to get count \n")
+consonants = []
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+alphernumeric = ['!', ',', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '=',
+                 '+', '/', '\\', ']']
+
+
+def letter_counter(paragraph):
+    array_of_letters(paragraph)
 
 
 def array_of_letters(sentence):
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-               'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    letters = alphabetsfn()
+    vowels = vowelsfn()
+    consonantsfn(letters, vowels)
 
-    heading = f'Letter ' \
-              f'Index ' \
-              f'Appearance '
+    heading = f'\tLetter ' \
+              f'\t\tIndex ' \
+              f'\t\t\tAppearance '
     print(heading)
 
     for letter in letters:
         if letter in sentence.lower():
-            final_result = f'   {letter}' \
-                           f'     {letters.index(letter) + 1}' \
-                           f'       {sentence.lower().count(letter, 0, len(sentence)+1)}'
+            final_result = f'\t   {letter}' \
+                           f'\t\t   {letters.index(letter) + 1}' \
+                           f'\t\t\t   {sentence.lower().count(letter, 0, len(sentence) + 1)}'
             print(final_result)
 
+    checkcase(sentence)
 
-letter_counter()
+    checknumbers(sentence)
+
+    checkalphernumeric(sentence)
+
+
+def alphabetsfn():
+    alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    print('Alphabetical letters are: ' + str(alphabets))
+    return alphabets
+
+
+def vowelsfn():
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    print('Vowels are: ' + str(vowel))
+    return vowel
+
+
+def consonantsfn(alphabets, vowels):
+    for leta in alphabets:
+        if leta not in vowels:
+            consonants.append(leta)
+    print('Consonants are: ' + str(consonants))
+
+
+def checkcase(paragraph):
+    upperCase = []
+    lowerCase = []
+
+    for letter in paragraph:
+        upperLetter = paragraph.upper()
+        lowerLetter = paragraph.lower()
+
+        if letter in upperLetter and letter.isalpha():
+            upperCase.append(letter)
+
+        elif letter in lowerLetter and letter.isalpha():
+            lowerCase.append(letter)
+
+    print('Letters in upper Case: ' + str(upperCase))
+    print('Letters in lower case: ' + str(lowerCase))
+
+
+def checknumbers(paragraph):
+    numbersFound = []
+
+    for number in paragraph:
+        if number.isdigit():
+            numbersFound.append(number)
+
+    print('numbers found are: ' + str(numbersFound))
+
+
+def checkalphernumeric(paragraph):
+    presentAlphanumerics = []
+    for symbol in paragraph:
+        if symbol.isalnum():
+            presentAlphanumerics.append(symbol)
+
+    print('alphanumerical characters: ' + str(presentAlphanumerics))
+
+
+letter_counter(para)
